5ab010fa62cae99376be63fcfb3a8aad60104d87
chris-Imade <jephthahimade@gmail.com>
2020-09-03 13:23:39 +0100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build a HULU CLONE Clever Programmers!</h1>
+      <Header />
+
+      {/* Nav */}
+
+      {/* Results */}
     </div>
   );
 }
