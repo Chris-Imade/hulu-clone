cf6bbc73bab7c5c604d86d385baba0dc850b7666
chris-Imade <jephthahimade@gmail.com>
2020-09-03 15:12:18 +0100

    commit

--- src/Header.js
+++ src/Header.js
@@ -7,7 +7,10 @@ function header() {
             <div class="header__icons" >
                 {/* All icons here */}
             </div>
-            img
+            <img 
+            src="https://press.hulu.com/wp-contents/uploads/2020/02/hulu-white.png"
+            alt=""
+            />
         </div>
     )
 }
