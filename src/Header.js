9033decf7e8f5898e3e589cdbe2bee896bb2443d
chris-Imade <jephthahimade@gmail.com>
2020-09-03 17:35:32 +0100

    new commit

--- src/Header.js
+++ src/Header.js
@@ -1,15 +1,50 @@
 import React from 'react';
 import "./header.css";
+import HomeIcon from '@material-ui/icons/Home';
+import FlashOnIcon from '@material-ui/icons/FlashOn';
+import LiveTvIcon from '@material-ui/icons/LiveTv';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import SearchIcon from '@material-ui/icons/Search';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 
 function header() {
     return (
         <div className="header">
-            <div class="header__icons" >
-                {/* All icons here */}
+            <div className="header__icons" >
+                <div className="header___icon">
+                <HomeIcon />
+                    <p>Home</p>
+                </div>
+
+                <div className="header___icon">
+                <FlashOnIcon /> 
+                    <p>Trending</p>
+                </div>
+
+                <div className="header___icon">
+                <LiveTvIcon />
+                    <p>Verified</p>
+                </div>
+
+                <div className="header___icon">
+                <VideoLibraryIcon />
+                    <p>Collections</p>
+                </div>
+
+                <div className="header___icon">
+                <SearchIcon />
+                    <p>Search</p>
+                </div>
+
+                <div className="header___icon">
+                <PersonOutlineIcon />
+                    <p>Account</p>
+                </div>
+                
             </div>
             <img 
-            src="https://press.hulu.com/wp-contents/uploads/2020/02/hulu-white.png"
-            alt=""
+            src="https://press.hulu.com/wp-content/uploads/2020/02/hulu-white.png"
+            alt="hulu Logo"
             />
         </div>
     )
