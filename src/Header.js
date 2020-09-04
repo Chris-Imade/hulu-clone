f2450123c74383e8671b5aaf78aab6fccc777d8f
chris-Imade <jephthahimade@gmail.com>
2020-09-04 10:37:09 +0100

    new commit

--- src/Header.js
+++ src/Header.js
@@ -11,32 +11,32 @@ function header() {
     return (
         <div className="header">
             <div className="header__icons" >
-                <div className="header___icon">
+                <div className="header__icon">
                 <HomeIcon />
                     <p>Home</p>
                 </div>
 
-                <div className="header___icon">
+                <div className="header__icon">
                 <FlashOnIcon /> 
                     <p>Trending</p>
                 </div>
 
-                <div className="header___icon">
+                <div className="header__icon">
                 <LiveTvIcon />
                     <p>Verified</p>
                 </div>
 
-                <div className="header___icon">
+                <div className="header__icon">
                 <VideoLibraryIcon />
                     <p>Collections</p>
                 </div>
 
-                <div className="header___icon">
+                <div className="header__icon">
                 <SearchIcon />
                     <p>Search</p>
                 </div>
 
-                <div className="header___icon">
+                <div className="header__icon">
                 <PersonOutlineIcon />
                     <p>Account</p>
                 </div>
